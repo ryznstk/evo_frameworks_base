07d8d6201aa0beed3c1b12ccb23600d0dedff531
rmp22 <195054967+rmp22@users.noreply.github.com>
2026-05-02 11:11:54 +0800

    core: Adding aod screen off animation
    Change-Id: I1c798bb0c29558c97fcf381d973189ce30af4a1e
    Signed-off-by: Ghosuto <clash.raja10@gmail.com>

--- packages/SystemUI/src/com/android/systemui/shade/QQSGestureListener.kt
+++ packages/SystemUI/src/com/android/systemui/shade/QQSGestureListener.kt
@@ -24,6 +24,8 @@ import android.view.MotionEvent
 import com.android.systemui.dagger.SysUISingleton
 import com.android.systemui.plugins.FalsingManager
 import com.android.systemui.plugins.statusbar.StatusBarStateController
+import com.android.systemui.user.domain.interactor.SelectedUserInteractor
+import com.android.systemui.power.domain.interactor.PowerInteractor
 import com.android.systemui.statusbar.StatusBarState
 import com.android.systemui.statusbar.phone.CentralSurfaces
 import lineageos.providers.LineageSettings
@@ -36,6 +38,7 @@ class QQSGestureListener @Inject constructor(
         private val powerManager: PowerManager,
         private val statusBarStateController: StatusBarStateController,
         private val centralSurfaces: CentralSurfaces,
+        private val powerInteractor: PowerInteractor,
 ) : GestureDetector.SimpleOnGestureListener() {
 
     private var doubleTapToSleepEnabled = false
@@ -71,7 +74,9 @@ class QQSGestureListener @Inject constructor(
                         !centralSurfaces.isBouncerShowing()) &&
                 !falsingManager.isFalseDoubleTap
         ) {
-            powerManager.goToSleep(e.getEventTime())
+            powerInteractor.setLastTouchToSleepPosition(e.x, e.y)
+            powerManager.goToSleep(e.eventTime,
+                PowerManager.GO_TO_SLEEP_REASON_TOUCH, 0)
             return true
         }
         return false
