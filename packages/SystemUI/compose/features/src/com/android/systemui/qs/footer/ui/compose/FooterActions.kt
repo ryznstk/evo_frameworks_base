Project-Xtended-FE/frameworks_base

SystemUI: Switch tile backgrounds based on blur support
Change-Id: I7e3f8f7e957d690b54703e5f96519cae569a64d5
Signed-off-by: Ghosuto <clash.raja10@gmail.com>

packages/SystemUI/compose/features/src/com/android/systemui/qs/footer/ui/compose/FooterActions.kt
@@ -89,6 +89,7 @@ import com.android.systemui.qs.footer.ui.viewmodel.FooterActionsDataUsageViewMod
 import com.android.systemui.qs.footer.ui.viewmodel.FooterActionsForegroundServicesButtonViewModel
 import com.android.systemui.qs.footer.ui.viewmodel.FooterActionsSecurityButtonViewModel
 import com.android.systemui.qs.footer.ui.viewmodel.FooterActionsViewModel
+import com.android.systemui.qs.panels.ui.compose.infinitegrid.CustomColorScheme
 import com.android.systemui.qs.ui.composable.QuickSettings
 import com.android.systemui.qs.ui.composable.QuickSettingsTheme
 import com.android.systemui.qs.ui.compose.borderOnFocus
@@ -407,7 +408,7 @@ private fun TextButton(
 ) {
     Expandable(
         shape = CircleShape,
-        color = LocalAndroidColorScheme.current.surfaceEffect1,
+        color = CustomColorScheme.current.qsTileColor,
         contentColor = MaterialTheme.colorScheme.onSurfaceVariant,
         modifier =
             modifier

ryznstk/evo_frameworks_base

packages/SystemUI/compose/features/src/com/android/systemui/qs/footer/ui/compose/FooterActions.kt
@@ -111,6 +111,7 @@ import com.android.systemui.qs.footer.ui.viewmodel.FooterActionsViewModel
 import com.android.systemui.qs.footer.ui.viewmodel.FooterTextButtonViewModel
 import com.android.systemui.qs.panels.ui.viewmodel.TextFeedbackViewModel
 import com.android.systemui.qs.shared.ui.QuickSettings
+import com.android.systemui.qs.panels.ui.compose.infinitegrid.CustomColorScheme
 import com.android.systemui.qs.ui.composable.QuickSettingsTheme
 import com.android.systemui.qs.ui.compose.borderOnFocus
 import com.android.systemui.res.R
@@ -732,7 +733,7 @@ private object FooterActionsDefaults {
     fun inactiveButtonColors(): ButtonColors =
         ButtonColors(
             icon = MaterialTheme.colorScheme.onSurface,
-            background = LocalAndroidColorScheme.current.surfaceEffect1,
+            background = CustomColorScheme.current.qsTileColor,
         )
 
     @Composable
@@ -740,7 +741,7 @@ private object FooterActionsDefaults {
     fun userSwitcherButtonColors(): ButtonColors =
         ButtonColors(
             icon = Color.Unspecified,
-            background = LocalAndroidColorScheme.current.surfaceEffect1,
+            background = CustomColorScheme.current.qsTileColor,
         )
 
     @Composable
@@ -748,7 +749,7 @@ private object FooterActionsDefaults {
     fun blurTextButtonColors(): TextButtonColors =
         TextButtonColors(
             content = MaterialTheme.colorScheme.onSurface,
-            background = LocalAndroidColorScheme.current.surfaceEffect1,
+            background = CustomColorScheme.current.qsTileColor,
             border = null,
         )
 